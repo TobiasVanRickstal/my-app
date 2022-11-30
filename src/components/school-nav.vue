5dd37d7916059f354dd1eaf92d9dfb2742f1dbe8
TobiasVanRickstal <tobias.vanrickstal@hotmail.com>
2022-11-30 14:39:14 +0100

    school nav

---
+++ src/components/school-nav.vue
@@ -0,0 +1,7 @@
+<template>
+    <router-link to="/"><img src="../assets/logo.png" alt="mainLogo"></router-link> |
+    <router-link to="/overzicht">overzicht</router-link>
+    <router-link to="/ons-bedrijf">ons-bedrijf</router-link>
+    <router-link to="/historiek">historiek</router-link>
+    <router-link to="/profile"><img src="../assets/logo.png" alt="profilePicture"></router-link>
+</template>
