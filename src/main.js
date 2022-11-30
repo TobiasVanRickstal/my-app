48e97529c7f23999e946265b18814a17c140a3d3
TobiasVanRickstal <tobias.vanrickstal@hotmail.com>
2022-11-30 14:37:37 +0100

    add firebase Config

--- src/main.js
+++ src/main.js
@@ -2,5 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyD_jZ5rOzC9ukOExVcpBpMEnivVJxOjceU",
+  authDomain: "schoolproject-e8e80.firebaseapp.com",
+  projectId: "schoolproject-e8e80",
+  storageBucket: "schoolproject-e8e80.appspot.com",
+  messagingSenderId: "778479687645",
+  appId: "1:778479687645:web:2359e33c719469f784c834"
+};
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).mount('#app').initializeApp(firebaseConfig)
