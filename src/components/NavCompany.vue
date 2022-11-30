bb113ddfe6dbaa40b596eb1989ea6730e2a4dd56
TobiasVanRickstal <tobias.vanrickstal@hotmail.com>
2022-11-30 14:44:51 +0100

    nev company update

---
+++ src/components/NavCompany.vue
@@ -0,0 +1,7 @@
+<template>
+    <router-link to="/"><img src="../assets/logo.png" alt="mainLogo"></router-link> |
+    <router-link to="/ons-aanbod">ons-aanbod</router-link>
+    <router-link to="/bedrijven-bekijken">bedrijven-bekijken</router-link>
+    <router-link to="/historiek">historiek</router-link>
+    <router-link to="/profile"><img src="../assets/logo.png" alt="profilePicture"></router-link>
+</template>
