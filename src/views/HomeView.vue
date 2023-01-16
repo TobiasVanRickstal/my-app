f6fc331347326e347e663ae99c8aea8ab316675c
TobiasVanRickstal <tobias.vanrickstal@hotmail.com>
2023-01-16 22:59:17 +0100

    offer door klikken

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,22 +3,15 @@
     <h1>Home</h1>
   </div>
   <div class="body">
-    <div class="form">
-      <Form />
-    </div>
   </div>
   
 </template>
 
 <script>
-import Form from '@/components/Form.vue';
 
 export default({
   setup() {
     
-  },
-  components: {
-    Form
   },
   data(){
     return{
